a120fac607695a76d33564a582269362a92ff4dc
Tyler Ruppert <tylerruppert@gmail.com>
2018-02-11 14:58:10 -0700

    App Metrics to InfluxDB
    * Sending app metrics for the example app to InfluxDB
    * Also updated es hosts in telegraf to get data on all hosts

---
+++ example-system/web-app/config/initializers/influxdb-rails.rb
@@ -0,0 +1,7 @@
+InfluxDB::Rails.configure do |config|
+  config.influxdb_database = "rails"
+  config.influxdb_username = "root"
+  config.influxdb_password = "root"
+  config.influxdb_hosts    = ["influxdb"]
+  config.influxdb_port     = 8086
+end
