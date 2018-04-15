ae70d265696df2cb509675f1dfe25aa904e8dee9
Tyler Ruppert <tylerruppert@gmail.com>
2018-04-15 11:12:58 -0600

    Preparing test framework
    * Deleted test files included with rails, and started setting up rspec.  Nothing built so far is complex enough to test.  Everything has been *magically* handled by Rails

--- clover-web/test/system/metric_sources_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class MetricSourcesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit metric_sources_url
-  #
-  #   assert_selector "h1", text: "MetricSource"
-  # end
-end
--- clover-web/test/system/thresholds_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class ThresholdsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit thresholds_url
-  #
-  #   assert_selector "h1", text: "Threshold"
-  # end
-end
