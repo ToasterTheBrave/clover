0ae8ba73df16cb9c99b9c29321039ef0449fe47c
Tyler Ruppert <tylerruppert@gmail.com>
2018-04-21 20:30:16 -0600

    Adding generate report as a url endpoint
    Right when the alert goes off, the report likely isn't quite ready.  Now, you can go to a url to have it generated, or regenerated.  The process of generating it only takes a few seconds.
    This requires a jar file from clover in order to work, but I don't want to track it in git.  It'll likely end up in S3, but I'm just leaving it untracked for now.

--- clover-web/app/controllers/reports_controller.rb
+++ clover-web/app/controllers/reports_controller.rb
@@ -8,6 +8,27 @@ class ReportsController < ApplicationController
 
   # GET /reports/1
   def show
+    @report_exists = File.file?("/home/truppert/projects/master-project/clover-web/public/reports/data/#{@report.timestamp}.js")
+  end
+
+  def generate
+    set_report
+
+    logger.info "/opt/spark/bin/spark-submit --class 'clover.service.BuildReport' /home/truppert/projects/master-project/clover-web/bin/clover.jar #{@report.timestamp}"
+    system("/opt/spark/bin/spark-submit --class 'clover.service.BuildReport' /home/truppert/projects/master-project/clover-web/bin/clover.jar #{@report.timestamp}")
+
+    success = false
+    (1..10).each do |i|
+      success = File.file?("/home/truppert/projects/master-project/clover-web/public/reports/data/#{@report.timestamp}.js")
+      break if success
+      sleep 5
+    end
+
+    if success
+      redirect_to @report, notice: 'Report is generated'
+    else
+      redirect_to @report, notice: 'Report generation failed'
+    end
   end
 
 
--- clover-web/config/routes.rb
+++ clover-web/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :metrics
   resources :metric_sources
   resources :reports, only: [:index, :show]
+  get 'reports/:id/generate', to: 'reports#generate', as: 'generate_report'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
